2a66e979d87f9fbedaed38b69bf4b3d195d706e8
Marc Fascia <mfascia@gmail.com>
2016-12-12 18:18:29 +0000

    Day 10 part 1

--- AoC_10.py
+++ AoC_10.py
@@ -8,7 +8,6 @@ def read_input_stream(filename):
     stream = map(lambda x: x.strip(" \n\t"), raw)
     return stream
 
-
 def main_1(input_stream):
     bots = {}
     outputs = {}
@@ -19,7 +18,7 @@ def main_1(input_stream):
         if line[:5] == "value":
             groups = re.match("value ([0-9]*) goes to bot ([0-9]*)", line).groups()
             if len(groups):
-                params = map(lambda x: int(x), groups)
+                params = [int(t) for t in groups]
                 if bots.has_key(params[1]):
                     bots[params[1]]["values"].append(params[0])
                     bots[params[1]]["values"].sort()
@@ -33,15 +32,15 @@ def main_1(input_stream):
         if line[:5] != "value":
             groups = re.match("bot ([0-9]*) gives low to (bot|output) ([0-9]*) and high to (bot|output) ([0-9]*)", line).groups()
             if len(groups):
-                s = int(groups[0])
-                d1 = int(groups[2]) * (1 if groups[1] == "bot" else -1)
-                d2 = int(groups[4]) * (1 if groups[3] == "bot" else -1)
-                if bots.has_key(s):
-                    bots[s]["rules"] = [d1, d2]
+                src = int(groups[0])
+                dst1 = int(groups[2]) * (1 if groups[1] == "bot" else -1)
+                dst2 = int(groups[4]) * (1 if groups[3] == "bot" else -1)
+                if bots.has_key(src):
+                    bots[src]["rules"] = [dst1, dst2]
                 else:
-                    bots[s] = {
+                    bots[src] = {
                         "values": [],
-                        "rules": [d1, d2]
+                        "rules": [dst1, dst2]
                     }
 
     propagate = True
@@ -54,26 +53,19 @@ def main_1(input_stream):
                 if item[1]["rules"][0] > 0:
                     bots[item[1]["rules"][0]]["values"].append(item[1]["values"][0])
                     bots[item[1]["rules"][0]]["values"].sort()
-                    if bots[item[1]["rules"][0]]["values"] == [17, 61]:
-                        print item
-                        break
                 else:
                     outputs[-1 * item[1]["rules"][0]] = item[1]["values"][0]
 
                 if item[1]["rules"][1] > 0:
                     bots[item[1]["rules"][1]]["values"].append(item[1]["values"][1])
                     bots[item[1]["rules"][1]]["values"].sort()
-                    if bots[item[1]["rules"][1]]["values"] == [17, 61]:
-                        print item
-                        break
                 else:
                     outputs[-1 * item[1]["rules"][1]] = item[1]["values"][1]
                 item[1]["values"] = []
                 propagate = True
         step += 1
 
-    # print json.dumps(bots, indent=4, sort_keys=True)
-    # print json.dumps(outputs, indent=4, sort_keys=True)
+    print json.dumps(outputs, indent=4, sort_keys=True)
                 
                 
             
