53ea3c94afd6af79f71553136898da20f49f4356
Marc Fascia <mfascia@gmail.com>
2016-12-19 19:11:32 +0000

    Day 19 part 1 solved theoretically

---
+++ AoC_19.py
@@ -0,0 +1,25 @@
+import sys
+
+nb_elves = 3001330
+
+
+def main_1():
+    # Theoretical answer: if n = n^a + b, solution is 2b+1. See https://www.youtube.com/watch?v=uCsD3ZGzMgE or lookup Josephus problem
+    # Dec to bin, drop the MSB, add 1 as a LSB, bin to dec, done! 
+    # For 3001330 elves, solution is 1808357
+    pass
+
+
+def main_2():
+    pass
+
+
+if __name__ == "__main__":
+    print ("Part 1 ---------------------------------------------------------------------------------------------------------")
+    main_1()
+    print
+    print ("Part 2 ---------------------------------------------------------------------------------------------------------")
+    main_2()
+
+'''
+'''
