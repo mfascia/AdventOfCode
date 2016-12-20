c7a61c4ed3b372379ed8ac97ba5f2a4c487299b4
Marc Fascia <mfascia@gmail.com>
2016-12-20 15:27:05 +0000

    Day 20 part 2

--- AoC_20.py
+++ AoC_20.py
@@ -8,7 +8,7 @@ def read_input(filename):
     return stream
 
 
-def main_1(text):
+def main(text):
     mins = []
     maxs = []
     for line in text:
@@ -22,6 +22,8 @@ def main_1(text):
     if mins[0] > 0:
         return 0
 
+    nb_allowed = 0
+    first_allowed = -1
     i = 0
     j = 0
     s = 0
@@ -32,9 +34,13 @@ def main_1(text):
         elif mins[i] > maxs[j]:
             s -= 1
             if s == 0 and mins[i] != maxs[j]+1:
-                print "i:", i, "j:",  "1st free:", str(maxs[j]+1)
-                break
+                if first_allowed == -1:
+                    first_allowed = maxs[j]+1
+                nb_allowed += mins[i] - (maxs[j]+1) 
             j += 1
+
+    print "part 1 - first allowed:", first_allowed
+    print "part 2 - nb allowed:", nb_allowed
 
 
 def main_2(text):
@@ -43,15 +49,30 @@ def main_2(text):
 
 if __name__ == "__main__":
     text = read_input("AoC_20_input.txt")
-    print ("Part 1 ---------------------------------------------------------------------------------------------------------")
-    print main_1(text)
-    
-    print
-    
-    print ("Part 2 ---------------------------------------------------------------------------------------------------------")
-    print main_2(text)
+    main(text)
 
 
 '''
+--- Day 20: Firewall Rules ---
 
+You'd like to set up a small hidden computer here so you can use it to get back into the network later. However, the corporate firewall only allows communication with certain external IP addresses.
+
+You've retrieved the list of blocked IPs from the firewall, but the list seems to be messy and poorly maintained, and it's not clear which IPs are allowed. Also, rather than being written in dot-decimal notation, they are written as plain 32-bit integers, which can have any value from 0 through 4294967295, inclusive.
+
+For example, suppose only the values 0 through 9 were valid, and that you retrieved the following blacklist:
+
+5-8
+0-2
+4-7
+
+The blacklist specifies ranges of IPs (inclusive of both the start and end value) that are not allowed. Then, the only IPs that this firewall allows are 3 and 9, since those are the only numbers not in any range.
+
+Given the list of blocked IPs you retrieved from the firewall (your puzzle input), what is the lowest-valued IP that is not blocked?
+
+Your puzzle answer was 32259706.
+--- Part Two ---
+
+How many IPs are allowed by the blacklist?
+
+Your puzzle answer was 113.
 '''
