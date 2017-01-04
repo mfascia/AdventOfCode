d73d4e93f1fadc822bef3eec3979dd41e285aacf
Marc Fascia <mfascia@gmail.com>
2017-01-04 19:11:42 +0000

    2015 day 2

--- template.py
+++ template.py
@@ -1,22 +1,28 @@
 import sys
 
 
-text = ""
+def read_input(filename):
+    with open(filename, "r") as f:
+        raw = f.readlines()
+    stream = map(lambda x: x.strip(" \n\t"), raw)
+    return stream
 
 
-def main_1():
+def main_1(text):
     pass
 
 
-def main_2():
+def main_2(text):
     pass
 
 
 if __name__ == "__main__":
+	in_text = read_input()
+
 	print ("Part 1 ---------------------------------------------------------------------------------------------------------")
-	main_1()
+	main_1(in_text)
 
 	print 
 
 	print ("Part 2 ---------------------------------------------------------------------------------------------------------")
-	main_2()
+	main_2(in_text)
