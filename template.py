f3fd8c6fab57c34d3a4ebef3a58b14a8fdaaa8b4
Marc Fascia <mfascia@gmail.com>
2018-12-05 12:07:29 +0000

    updated and cleaned up the template

--- template.py
+++ template.py
@@ -1,28 +1,73 @@
+import os
 import sys
 
 
+# GLOBALS --------------------------------------------------------------------------------------
+
+# for short input sets, it can be declared here instead of in seperate files
+# otherwise, tests go in files named AoC_xx_test_x.txt and input goes in AoC_xx_input.txt
+tests = []
+inp = ""
+
+doTests = True
+doInput = True
+enablePart2 = True
+#-----------------------------------------------------------------------------------------------
+
+
+def main_1(inp):
+	pass
+
+
+def main_2(inp):
+	pass
+
+
 def read_input(filename):
-    with open(filename, "r") as f:
-        raw = f.readlines()
-    stream = map(lambda x: x.strip(" \n\t"), raw)
-    return stream
-
-
-def main_1(text):
-    pass
-
-
-def main_2(text):
-    pass
-
+	with open(filename, "r") as f:
+		raw = f.readlines()
+	stream = map(lambda x: x.strip(" \n\t"), raw)
+	return stream
+	
 
 if __name__ == "__main__":
-	in_text = read_input()
+	if doTests:
+		# read tests
+		if len(tests) == 0:
+			i = 0
+			while True:
+				i += 1
+				testfile = sys.argv[0].replace(".py", ("_test_%d.txt" % i))
+				if os.path.isfile(testfile):
+					tests.append(read_input(testfile))
+				else:
+					break
+		
+	if doInput:
+		# read input
+		if len(inp) == 0:
+			inp = read_input(sys.argv[0].replace(".py", "_input.txt"))
 
-	print ("Part 1 ---------------------------------------------------------------------------------------------------------")
-	main_1(in_text)
+	if doTests:
+		# run tests
+		print "--------------------------------------------------------------------------------"
+		print "- TESTS"
+		print "--------------------------------------------------------------------------------"
+		for t in xrange(0, len(tests)):
+			print "--- Test #" + str(t) + ".1 ------------------------------"
+			main_1(tests[t])
+			if enablePart2:
+				print "--- Test #" + str(t) + ".2 ------------------------------"
+				main_2(tests[t])
+			print 
 
-	print 
-
-	print ("Part 2 ---------------------------------------------------------------------------------------------------------")
-	main_2(in_text)
+	if doInput:
+		# process input
+		print "--------------------------------------------------------------------------------"
+		print "- INPUT"
+		print "--------------------------------------------------------------------------------"
+		print "--- Part 1 ------------------------------"
+		main_1(inp)
+		if enablePart2:
+			print "--- Part 1 ------------------------------"
+			main_2(inp)
