f327537fab1b0cdd72dcd0d9f643b699a2657439
Marc Fascia <mfascia@gmail.com>
2021-12-06 12:24:27 +0000

    2021 day 6

---
+++ 2021/AoC_06.py
@@ -0,0 +1,93 @@
+import os
+import sys
+
+
+# GLOBALS --------------------------------------------------------------------------------------
+
+# for short input sets, it can be declared here instead of in seperate files
+# otherwise, tests go in files named AoC_xx_test_x.txt and input goes in AoC_xx_input.txt
+tests = ["3,4,3,1,2"]
+inp = "2,5,5,3,2,2,5,1,4,5,2,1,5,5,1,2,3,3,4,1,4,1,4,4,2,1,5,5,3,5,4,3,4,1,5,4,1,5,5,5,4,3,1,2,1,5,1,4,4,1,4,1,3,1,1,1,3,1,1,2,1,3,1,1,1,2,3,5,5,3,2,3,3,2,2,1,3,1,3,1,5,5,1,2,3,2,1,1,2,1,2,1,2,2,1,3,5,4,3,3,2,2,3,1,4,2,2,1,3,4,5,4,2,5,4,1,2,1,3,5,3,3,5,4,1,1,5,2,4,4,1,2,2,5,5,3,1,2,4,3,3,1,4,2,5,1,5,1,2,1,1,1,1,3,5,5,1,5,5,1,2,2,1,2,1,2,1,2,1,4,5,1,2,4,3,3,3,1,5,3,2,2,1,4,2,4,2,3,2,5,1,5,1,1,1,3,1,1,3,5,4,2,5,3,2,2,1,4,5,1,3,2,5,1,2,1,4,1,5,5,1,2,2,1,2,4,5,3,3,1,4,4,3,1,4,2,4,4,3,4,1,4,5,3,1,4,2,2,3,4,4,4,1,4,3,1,3,4,5,1,5,4,4,4,5,5,5,2,1,3,4,3,2,5,3,1,3,2,2,3,1,4,5,3,5,5,3,2,3,1,2,5,2,1,3,1,1,1,5,1"
+
+doTests = True
+doInput = True
+enablePart1 = True
+enablePart2 = True
+#-----------------------------------------------------------------------------------------------
+
+
+def evolve(inp, end):
+	values = [int(x) for x in inp.split(",")]
+	days = [0 for x in range(0, 9)]
+
+	for v in values:
+		days[v] += 1
+
+	end_day = end
+
+	for d in range(1, end_day+1):
+		next = days[1:]
+		next.append(days[0])
+		next[6] += days[0]
+		days = next
+	print(d, sum(days), days)
+
+
+def main_1(inp):
+	evolve(inp, 80)
+
+
+def main_2(inp):
+	evolve(inp, 256)
+
+
+def read_input(filename):
+	with open(filename, "r") as f:
+		raw = f.readlines()
+	stream = map(lambda x: x.strip(" \n\t"), raw)
+	return stream
+	
+
+if __name__ == "__main__":
+	if doTests:
+		# read tests
+		if len(tests) == 0:
+			i = 0
+			while True:
+				i += 1
+				testfile = sys.argv[0].replace(".py", ("_test_%d.txt" % i))
+				if os.path.isfile(testfile):
+					tests.append([x for x in read_input(testfile)])
+				else:
+					break
+		
+	if doInput:
+		# read input
+		if len(inp) == 0:
+			inp = [x for x in read_input(sys.argv[0].replace(".py", "_input.txt"))]
+
+	if doTests:
+		# run tests
+		print ("--------------------------------------------------------------------------------")
+		print ("- TESTS")
+		print ("--------------------------------------------------------------------------------")
+		for t in range(0, len(tests)):
+			if enablePart1:
+				print ("--- Test #" + str(t+1) + ".1 ------------------------------")
+				main_1(tests[t])
+			if enablePart2:
+				print ("--- Test #" + str(t+1) + ".2 ------------------------------")
+				main_2(tests[t])
+			print ()
+
+	if doInput:
+		# process input
+		print ("--------------------------------------------------------------------------------")
+		print ("- INPUT")
+		print ("--------------------------------------------------------------------------------")
+		if enablePart1:
+			print ("--- Part 1 ------------------------------")
+			main_1(inp)
+		if enablePart2:
+			print ("--- Part 2 ------------------------------")
+			main_2(inp)
