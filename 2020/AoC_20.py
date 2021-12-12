47e8854c6142541f28fd878de13341762e1a1c7e
Marc Fascia <mfascia@gmail.com>
2021-12-12 13:21:20 +0000

    2020 day 20 part 1

--- 2020/AoC_20.py
+++ 2020/AoC_20.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import json
+
 
 
 # GLOBALS --------------------------------------------------------------------------------------
@@ -10,14 +12,68 @@ tests = []
 inp = ""
 
 doTests = True
-doInput = False
+doInput = True
 enablePart1 = True
 enablePart2 = False
 #-----------------------------------------------------------------------------------------------
 
+def nbMatchedEdges(tile):
+	score = 0
+	for e in tile["matches"]:
+		if len(e) > 0:
+			score += 1
+		if len(e)>1:
+			print(json.dumps(values, indent=4))
+	return score
+
 
 def main_1(inp):
-	pass
+	tiles = {}
+	for i in range(0, len(inp), 12):
+		line = inp[i]
+		tile = {}
+		_, id = line.split(" ")
+		tile["id"] = int(id[:-1])
+		tile["raw_data"] = "".join(inp[i+1:i+11])
+		tile["bin_data"] = tile["raw_data"].replace(".", "0").replace("#", "1")
+		tile["edges"] = [] # N,rN, S,rS, E,rE, W,rW
+		
+		binStr = tile["bin_data"][0:10]
+		tile["edges"].append(int(binStr,2))
+		tile["edges"].append(int(binStr[::-1],2))
+
+		binStr = tile["bin_data"][90:]
+		tile["edges"].append(int(binStr,2))
+		tile["edges"].append(int(binStr[::-1],2))
+
+		binStr = "".join([tile["bin_data"][x+9] for x in range(0, 100, 10)])
+		tile["edges"].append(int(binStr,2))
+		tile["edges"].append(int(binStr[::-1],2))
+
+		binStr = "".join([tile["bin_data"][x] for x in range(0, 100, 10)])	
+		tile["edges"].append(int(binStr,2))
+		tile["edges"].append(int(binStr[::-1],2))
+		tile["matches"] = [[] for x in range(0,8)]
+
+		tiles[tile["id"]] = tile
+
+	values = [v for v in tiles.values()]
+	for i in range(0, len(values)):
+		for j in range(i+1, len(values)):
+			for a in range(0, 8):
+				for b in range(0, 8):
+					if values[i]["edges"][a] == values[j]["edges"][b]:
+						values[i]["matches"][a].append(values[j]["id"])
+						values[j]["matches"][b].append(values[i]["id"])
+						
+	for i in range(0, len(values)):
+		values[i]["Nb_mathce_edges"] = 0
+
+	values = sorted(values, key=lambda x: nbMatchedEdges(x))
+	#print(values[0:4])
+	print(values[0]["id"] * values[1]["id"] * values[2]["id"] * values[3]["id"])
+	#print(json.dumps(values, indent=4))
+
 
 
 def main_2(inp):
@@ -30,7 +86,7 @@ def read_input(filename):
 	stream = map(lambda x: x.strip(" \n\t"), raw)
 	return stream
 	
-
+ 
 if __name__ == "__main__":
 	if doTests:
 		# read tests
