11231e1c3cda79d923317e474802f23e2d235957
Marc Fascia <mfascia@gmail.com>
2021-12-22 22:48:50 +0000

    2021 day 22 part 1, need optim for part 2!

---
+++ 2021/AoC_22.py
@@ -0,0 +1,192 @@
+import os
+import sys
+import re
+
+
+# GLOBALS --------------------------------------------------------------------------------------
+
+# for short input sets, it can be declared here instead of in seperate files
+# otherwise, tests go in files named AoC_xx_test_x.txt and input goes in AoC_xx_input.txt
+tests = []
+inp = ""
+isTest = False
+
+doTests = False
+doInput = True
+enablePart1 = True
+enablePart2 = False
+#-----------------------------------------------------------------------------------------------
+
+X = 0
+Y = 1
+Z = 2
+
+class Box:
+	def __init__(self, x1, x2, y1, y2, z1, z2):
+		self.min = (min(x1, x2), min(y1, y2), min(z1, z2))
+		self.max = (max(x1, x2), max(y1, y2), max(z1, z2))
+
+	def __str__(self):
+		return f"[{self.min} --> {self.max}]"
+
+	def volume(self):
+		return (self.max[0]-self.min[0]) * (self.max[1]-self.min[1]) * (self.max[2]-self.min[2])
+
+	def split(self, v, axis):
+		if self.min[axis] < v and v < self.max[axis]:
+			if axis == X:
+				a = Box(self.min[0], v, self.min[1], self.max[1], self.min[2], self.max[2])
+				b = Box(v, self.max[0], self.min[1], self.max[1], self.min[2], self.max[2])
+			elif axis == Y:
+				a = Box(self.min[0], self.max[0], self.min[1], v, self.min[2], self.max[2])
+				b = Box(self.min[0], self.max[0], v, self.max[1], self.min[2], self.max[2])
+			elif axis == Z:
+				a = Box(self.min[0], self.max[0], self.min[1], self.max[1], self.min[2], v)
+				b = Box(self.min[0], self.max[0], self.min[1], self.max[1], v, self.max[2])
+			return [a, b]
+		else:
+			return [self]
+
+	def cut_by(self, other):
+		boxes = [self]
+		for a in range(0,3):
+			sub = []
+			for b in boxes:
+				sub += b.split(other.min[a], a)
+			boxes = sub
+			sub = []
+			for b in boxes:
+				sub += b.split(other.max[a], a)
+			boxes = sub
+		return boxes
+
+	def contains(self, other):
+		return self.min[X] <= other.min[X] and self.max[X] >= other.max[X] and self.min[Y] <= other.min[Y] and self.max[Y] >= other.max[Y] and self.min[Z] <= other.min[Z] and self.max[Z] >= other.max[Z]
+
+
+
+	def tests():
+		a = Box(0, 10, 0, 10, 0, 10)
+		b = Box(5, 15, 5, 15, 5, 15)
+		c = Box(3, 3, 3, 7, 7, 7)
+		print(a)
+		print(b)
+
+		pieces = b.cut_by(a)
+		for p in pieces:
+			print(p)
+ 
+		print(a.contains(b))
+		print(a.contains(c))
+		print(a.contains(a))
+		print(c.contains(a))
+
+
+def main_1(inp):
+	bounds = Box(-50, 51, -50, 51, -50, 51)
+	boxes = []
+	for line in inp:
+		match = re.match("(on|off) x=([-,0-9]*)\.\.([-,0-9]*),y=([-,0-9]*)\.\.([-,0-9]*),z=([-,0-9]*)\.\.([-,0-9]*)", line)
+		groups = match.groups()
+		if groups:
+			box = Box(int(groups[1]), int(groups[2])+1, int(groups[3]), int(groups[4])+1, int(groups[5]), int(groups[6])+1)
+			# if not bounds.contains(box):
+			# 	print("Out of bouds: ", line, "SKIPPED")
+			# 	continue
+			if len(boxes) == 0 and groups[0] == "on":
+				boxes.append(box)
+			elif groups[0] == "on":
+				pieces = [box]
+				# clip the new box with all the existing boxes that are on, and keep only the new parts
+				for b in boxes:
+					newPieces = []
+					for p in pieces:
+						if b.contains(p):
+							continue
+						splits = p.cut_by(b)
+						for s in splits:
+							if not b.contains(s):
+								newPieces.append(s)
+					pieces = newPieces
+				boxes += pieces
+			elif groups[0] == "off":
+				replaceList = []
+				for b in boxes:
+					if box.contains(b):
+						replaceList.append([b, []])
+					else:
+						splits = b.cut_by(box)
+						if len(splits) > 1:
+							remains = []
+							for s in splits:
+								if not box.contains(s):
+									remains.append(s)
+							replaceList.append([b, remains])
+				for r in replaceList:
+					boxes.remove(r[0])
+					boxes += r[1]
+			volume = 0
+			for b in boxes:
+				volume += b.volume()
+			print("# boxes:", len(boxes), ", volume:", volume)	
+		else:
+			print("could not parse:", line)
+
+
+
+def main_2(inp):
+	pass
+
+
+def read_input(filename):
+	with open(filename, "r") as f:
+		raw = f.readlines()
+	stream = map(lambda x: x.strip(" \n\t"), raw)
+	return stream
+	
+
+if __name__ == "__main__":
+	if doTests:
+		# read tests
+		if len(tests) == 0:
+			i = 0
+			while True:
+				i += 1
+				testfile = sys.argv[0].replace(".py", ("_test_%d.txt" % i))
+				if os.path.isfile(testfile):
+					tests.append([x for x in read_input(testfile)])
+				else:
+					break
+		
+	if doInput:
+		# read input
+		if len(inp) == 0:
+			inp = [x for x in read_input(sys.argv[0].replace(".py", "_input.txt"))]
+
+	if doTests:
+		# run tests
+		isTest = True
+		print ("--------------------------------------------------------------------------------")
+		print ("- TESTS")
+		print ("--------------------------------------------------------------------------------")
+		for t in range(0, len(tests)):
+			if enablePart1:
+				print ("--- Test #" + str(t+1) + ".1 ------------------------------")
+				main_1(tests[t])
+			if enablePart2:
+				print ("--- Test #" + str(t+1) + ".2 ------------------------------")
+				main_2(tests[t])
+			print ()
+
+	if doInput:
+		# process input
+		isTest = False
+		print ("--------------------------------------------------------------------------------")
+		print ("- INPUT")
+		print ("--------------------------------------------------------------------------------")
+		if enablePart1:
+			print ("--- Part 1 ------------------------------")
+			main_1(inp)
+		if enablePart2:
+			print ("--- Part 2 ------------------------------")
+			main_2(inp)
