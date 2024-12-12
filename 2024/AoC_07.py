7ff0d64e0f21281811422bb51a7edf5e35675974
Marc Fascia <mfascia@gmail.com>
2024-12-12 13:05:48 +0000

    2024 day 7 part 1

---
+++ 2024/AoC_07.py
@@ -0,0 +1,112 @@
+import os
+import sys
+
+
+# GLOBALS --------------------------------------------------------------------------------------
+
+# for short input sets, it can be declared here instead of in seperate files
+# otherwise, tests go in files named AoC_xx_test_x.txt and input goes in AoC_xx_input.txt
+tests = []
+inp = ""
+isTest = False
+
+doTests = True
+doInput = False
+enablePart1 = True
+enablePart2 = True
+#-----------------------------------------------------------------------------------------------
+
+def eval(target, lhs, rhs):
+	if len(rhs) == 0:
+		return lhs == target
+	elif len(rhs) == 1:
+		if lhs + rhs[0] == target:
+			return True
+		elif lhs * rhs[0] == target:
+			return True
+		else:
+			return False
+	else:
+		v = rhs[0]
+		rhs = rhs[1:]
+		if eval(target, lhs + v, rhs):
+			return True
+		else:
+			return eval(target, lhs * v, rhs)
+
+
+def main_1(inp):
+	sum = 0
+	for line in inp:
+		result, rhs = line.split(": ")
+		result = int(result)
+		args = [int(x) for x in rhs.split(" ")]
+
+		if eval( result, args[0], args[1:]):
+			sum += result
+
+	print(sum)
+
+
+def main_2(inp):
+	sum = 0
+	for line in inp:
+		result, rhs = line.split(": ")
+		result = int(result)
+		args = [int(x) for x in rhs.split(" ")]
+
+	print(sum)
+
+
+def read_input(filename):
+	with open(filename, "r") as f:
+		raw = f.readlines()
+	stream = map(lambda x: x.strip(" \n\t"), raw)
+	return stream
+	
+
+if __name__ == "__main__":
+	if doTests:
+		# read tests
+		if len(tests) == 0:
+			i = 0
+			while True:
+				i += 1
+				testfile = sys.argv[0].replace(".py", ("_test_%d.txt" % i))
+				if os.path.isfile(testfile):
+					tests.append([x for x in read_input(testfile)])
+				else:
+					break
+		
+	if doInput:
+		# read input
+		if len(inp) == 0:
+			inp = [x for x in read_input(sys.argv[0].replace(".py", "_input.txt"))]
+
+	if doTests:
+		# run tests
+		isTest = True
+		print ("--------------------------------------------------------------------------------")
+		print ("- TESTS")
+		print ("--------------------------------------------------------------------------------")
+		for t in range(0, len(tests)):
+			if enablePart1:
+				print ("--- Test #" + str(t+1) + ".1 ------------------------------")
+				main_1(tests[t])
+			if enablePart2:
+				print ("--- Test #" + str(t+1) + ".2 ------------------------------")
+				main_2(tests[t])
+			print ()
+
+	if doInput:
+		# process input
+		isTest = False
+		print ("--------------------------------------------------------------------------------")
+		print ("- INPUT")
+		print ("--------------------------------------------------------------------------------")
+		if enablePart1:
+			print ("--- Part 1 ------------------------------")
+			main_1(inp)
+		if enablePart2:
+			print ("--- Part 2 ------------------------------")
+			main_2(inp)
